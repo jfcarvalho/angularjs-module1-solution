5bcaec350c23a598c24344444f2a177099d1d5ff
Jean Carvalho <Jean Carvalho>
2016-10-29 20:26:44 -0300

    Esqueci de tratar caso vaziogit add .

--- assignment1/assignment1-starter-code/app.js
+++ assignment1/assignment1-starter-code/app.js
@@ -5,14 +5,19 @@ angular.module('Assignment1', [])
 
 .controller('LunchController', function ($scope) {
   $scope.sayHello = function () {
-      var modal = document.getElementsByClassName('message');
-      var llist = $scope.lunchlist.split(",");
-      if(llist.length <= 3 && llist.length >= 0)
+      if($scope.lunchlist) {
+        var llist = $scope.lunchlist.split(",");
+        if(llist.length <= 3 && llist.length > 0)
+        {
+          $scope.msg = "Enjoy!";
+        }
+        else if (llist.length > 3 ){
+          $scope.msg = "Too much!";
+        }
+    }
+      else
       {
-        $scope.msg = "Enjoy!";
-      }
-      else if (llist.length > 3 ){
-        $scope.msg = "Too much!";
+          $scope.msg = "Please enter data first";
       }
   };
 });
