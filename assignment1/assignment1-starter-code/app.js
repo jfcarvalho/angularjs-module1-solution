087b2239b74db4b5b62704cdd5cf08bd24e63e6b
Jean Carvalho <Jean Carvalho>
2016-10-29 20:08:13 -0300

    Comentario avisando que considera vazio como item

--- assignment1/assignment1-starter-code/app.js
+++ assignment1/assignment1-starter-code/app.js
@@ -4,21 +4,16 @@
 angular.module('Assignment1', [])
 
 .controller('LunchController', function ($scope) {
-//  $scope.name = "Yaakov";
   $scope.sayHello = function () {
       var modal = document.getElementsByClassName('message');
-      //modal.find('.message').html($scope.lunchlist);
-      //return modal.html($scope.lunchlist);
       var llist = $scope.lunchlist.split(",");
       if(llist.length <= 3 && llist.length >= 0)
       {
-        $scope.msg = "Enjoy!"
+        $scope.msg = "Enjoy!";
       }
       else if (llist.length > 3 ){
-        $scope.msg = "Too much!"
+        $scope.msg = "Too much!";
       }
-    //  $scope.msg = llist.length;
-
   };
 });
 
